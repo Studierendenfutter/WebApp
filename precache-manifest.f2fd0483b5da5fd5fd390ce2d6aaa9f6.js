7d9eb15da1f33b653a219cb56450fc247e3a8966
PaulsBecks <paul.beck.berlin@gmail.com>
2020-07-31 15:56:43 +0200

    Updates

---
+++ precache-manifest.f2fd0483b5da5fd5fd390ce2d6aaa9f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3f5e28999a9d0b91fd2c177a591d394",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76fb7499eb79093e1480",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "37a30d56561bf74e2de7",
+    "url": "/static/js/2.e6ceb916.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e6ceb916.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76fb7499eb79093e1480",
+    "url": "/static/js/main.75b7ee4f.chunk.js"
+  },
+  {
+    "revision": "61b7409867d0aaa1f0eb",
+    "url": "/static/js/runtime-main.63bf9351.js"
+  }
+]);
