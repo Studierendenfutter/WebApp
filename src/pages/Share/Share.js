2cf6fa33377647f5aa9cc81b76c760f8d34cb275
PaulsBecks <paul.beck.berlin@gmail.com>
2020-10-13 10:12:48 +0200

    added to clipboard

--- src/pages/Share/Share.js
+++ src/pages/Share/Share.js
@@ -29,13 +29,18 @@ export default function Share() {
     text:
       "Hey, mit dem Lunchletter von Studierendenfutter bekomme ich jeden Tag mein persönliches Mittagsmenü zugeschickt. Wenn du dich auch anmelden möchtest, kannst du das hier tun:",
     url: "https://www.studierendenfutter.de",
-    style: { margin: "1em", },
+    style: { margin: "1em" },
   };
 
   useEffect(() => {
     async function share() {}
     share();
   }, []);
+
+  const copyText = () => {
+    const text = shareData.text + " " + shareData.url;
+    navigator.clipboard.writeText(text);
+  };
 
   return (
     <div
@@ -70,6 +75,17 @@ export default function Share() {
         <RedditShareButton {...shareData}>
           <RedditIcon size={115} round={true} iconFillColor="#FFFCDC" />
         </RedditShareButton>
+        <div className="sf-copy-button" onClick={copyText}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="115"
+            viewBox="0 0 37 37"
+            width="115"
+          >
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z" />
+          </svg>
+        </div>
       </div>
     </div>
   );
