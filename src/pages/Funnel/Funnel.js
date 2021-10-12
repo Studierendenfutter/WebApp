a835e15829e40e511be73b20d4b1c38883508ab5
Paul Beck <paulbeck@Pauls-MacBook-Pro.local>
2021-10-12 09:21:06 +0200

    remove semesterferien question

--- src/pages/Funnel/Funnel.js
+++ src/pages/Funnel/Funnel.js
@@ -186,10 +186,10 @@ export default function Funnel() {
       )}
       {funnelStep === 2 && (
         <FunnelStep title="Auch in den Semesterferien?">
-          <p>
+          {/*<p>
             Bis zum 11.10. sind noch Semesterferien. Wenn du den Lunchletter ab
             sofort erhalten möchtest, wähle hier "Ja" aus.
-          </p>
+          </p>*/}
           <div>
             <button
               className="sf-funnel-big-button"
